7da6178ade6954670bf6be1ab8fa1110859d58cd
Greg Garville Jr <gjgarvil@gmail.com>
2017-10-03 09:54:18 -0400

    stylying new shuttle page

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,4 @@
 class Test < ApplicationRecord
   ORIGIN_LIST = ["Cape Canaveral, Florida", "Van Horn, Texas", "Vandenberg AFB, California"]
+  DESTINATION_LIST = ["Hellas Basin", "Arabia Terra", "Valles Marineris", "Columbia Hills" ]
 end
